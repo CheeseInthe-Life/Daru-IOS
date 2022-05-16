54d7f3a4c9d89bd984226d5de54bc59d968dcb85
zaezero97 <xornjs1423@gmail.com>
2022-05-17 01:18:02 +0900

    :lipstick: color asset 추가
    color asset 추가

---
+++ Daru/Sources/Presentation/Common/Extensions/UIColor+.swift
@@ -0,0 +1,32 @@
+//
+//  UIColor.swift
+//  Daru
+//
+//  Created by 재영신 on 2022/05/17.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    class var gray1: UIColor? { return UIColor(named: "Gray1") }
+    class var gray2: UIColor? { return UIColor(named: "Gray2") }
+    class var gray3: UIColor? { return UIColor(named: "Gray3") }
+    class var gray4: UIColor? { return UIColor(named: "Gray4") }
+    class var gray5: UIColor? { return UIColor(named: "Gray5") }
+    class var gray6: UIColor? { return UIColor(named: "Gray6") }
+    class var gray7: UIColor? { return UIColor(named: "Gray7") }
+    
+    class var brown1: UIColor? { return UIColor(named: "Brown1") }
+    class var brown2: UIColor? { return UIColor(named: "Brown2") }
+    class var brown3: UIColor? { return UIColor(named: "Brown3") }
+    
+    class var blueGreen1: UIColor? { return UIColor(named: "BlueGreen1") }
+    class var blueGreen2: UIColor? { return UIColor(named: "BlueGreen2") }
+    
+    class var yellowGreen1: UIColor? { return UIColor(named: "YellowGreen1") }
+    class var yellowGreen2: UIColor? { return UIColor(named: "YellowGreen2") }
+    
+    class var navy1: UIColor? { return UIColor(named: "Navy1") }
+    class var navy2: UIColor? { return UIColor(named: "Navy2") }
+}
