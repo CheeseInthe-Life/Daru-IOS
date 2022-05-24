0d53a0d2cff1381292dcb1487913d6b3b336cad4
zaezero97 <xornjs1423@gmail.com>
2022-05-24 23:00:49 +0900

    [#11] Feat: FavoriteTeahouse Layout 구현

---
+++ Daru/Sources/Presentation/MyDaruScene/Cells/FavoriteTeaHouseHeaderView.swift
@@ -0,0 +1,41 @@
+//
+//  FavoriteTeaHouseHeaderView.swift
+//  Daru
+//
+//  Created by 재영신 on 2022/05/24.
+//
+
+import UIKit
+import SnapKit
+import Then
+
+final class FavoriteTeaHouseHeaderView: UICollectionReusableView {
+    
+    static let identifier = "FavoriteTeaHouseHeaderView"
+    
+    private let mainLabel = UnderLineLabel(color: .brown3!, weight: 10.0).then {
+        $0.font = .notoSansKR(.bold, size: 20.0)
+        $0.text = "내가 좋아하는 찻집"
+        $0.textAlignment = .center
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    private func configureUI() {
+        
+        addSubview(mainLabel)
+        
+        mainLabel.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.bottom.equalToSuperview().inset(20.0)
+        }
+        
+    }
+}
