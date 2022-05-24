7cf5ae94865ad36d80561cc3108e1b7bf71cee19
zaezero97 <xornjs1423@gmail.com>
2022-05-25 01:17:59 +0900

    [#18] Feat: postListTableView, WriteButton 추가

--- Daru/Sources/Presentation/CommunityScene/PostListViewController.swift
+++ Daru/Sources/Presentation/CommunityScene/PostListViewController.swift
@@ -14,9 +14,43 @@ import RxCocoa
 
 final class PostListViewController: BaseViewController, Stepper {
     
+    private let writeButton = UIButton().then {
+        $0.backgroundColor = .brown2
+        $0.layer.cornerRadius = 4.0
+        $0.setTitle("글 작성하기", for: .normal)
+        $0.setTitleColor(.white, for: .normal)
+    }
+    
+    private let postListTableView = UITableView().then {
+        $0.showsVerticalScrollIndicator = false
+    }
+    
     var steps: PublishRelay<Step> = .init()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    override func configureUI() {
+        super.configureUI()
+        
+        [
+            writeButton,
+            postListTableView
+        ].forEach {
+            view.addSubview($0)
+        }
+        
+        writeButton.snp.makeConstraints { make in
+            make.trailing.equalToSuperview().inset(20.0)
+            make.top.equalToSuperview().inset(15.0)
+            make.width.equalTo(88.0)
+            make.height.equalTo(25.0)
+        }
+        
+        postListTableView.snp.makeConstraints { make in
+            make.leading.trailing.bottom.equalToSuperview()
+            make.top.equalTo(writeButton.snp.bottom).offset(14.0)
+        }
     }
 }
