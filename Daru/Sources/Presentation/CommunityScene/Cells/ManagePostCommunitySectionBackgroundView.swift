f8f4b0682af006e56fa3f0593521ef2331c57455
zaezero97 <xornjs1423@gmail.com>
2022-05-29 18:59:48 +0900

    [#19] Feat: ManagePostScene Community Section Item간의 구분선 추가

--- Daru/Sources/Presentation/CommunityScene/Cells/ManagePostCommunitySectionBackgroundView.swift
+++ Daru/Sources/Presentation/CommunityScene/Cells/ManagePostCommunitySectionBackgroundView.swift
@@ -26,6 +26,22 @@ final class ManagePostCommunitySectionBackgroundView: UICollectionReusableView {
         backgroundColor = .gray7?.withAlphaComponent(0.7)
         layer.borderWidth = 1.0
         layer.borderColor = UIColor.gray5?.cgColor
+        
+        let path = UIBezierPath()
+        path.move(to: CGPoint(x: bounds.minX + 20.0, y: bounds.midY))
+        path.addLine(to: CGPoint(x: bounds.maxX - 20.0, y: bounds.midY))
+        path.move(to: CGPoint(x: bounds.midX, y: bounds.minY + 20.0))
+        path.addLine(to: CGPoint(x: bounds.midX, y: bounds.midY - 20.0))
+        path.move(to: CGPoint(x: bounds.midX, y: bounds.midY + 20.0))
+        path.addLine(to: CGPoint(x: bounds.midX, y: bounds.maxY - 20.0))
+        
+        let shapeLayer = CAShapeLayer()
+        shapeLayer.path = path.cgPath
+        shapeLayer.lineWidth = 3.0
+        shapeLayer.strokeColor = UIColor.gray5?.cgColor
+        shapeLayer.lineDashPattern = [3,3]
+        layer.addSublayer(shapeLayer)
+        
     }
     
 }
