a6089032791cfec81a747a53218e75a69919443a
zaezero97 <xornjs1423@gmail.com>
2022-05-29 20:49:36 +0900

    [#27] Feat: NearTeahouseViewController 생성

---
+++ Daru/Sources/Presentation/HomeScene/NearTeahouseViewController.swift
@@ -0,0 +1,21 @@
+//
+//  NearTeahouseViewController.swift
+//  Daru
+//
+//  Created by 재영신 on 2022/05/29.
+//
+
+import UIKit
+import SnapKit
+import Then
+
+final class NearTeahouseViewController: BaseViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func configureUI() {
+        super.configureUI()
+    }
+}
